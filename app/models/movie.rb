a689e97470c60efbdc4e1286ea11551fa1f0f85f
Brandon Harris <cheeridon@gmail.com>
2017-10-20 15:39:02 -0400

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+  #attr_accessor :title, :release_date, :director, :lead, :in_theaters
+
+  #def initialize(attributes = {})
+  #  attributes.each {|key, value| self.send(("#{key}="),value)}
+  #end
 end
